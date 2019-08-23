696652df5bfafb37411f26746f6c72cf594db777
dev <48061063+franzs97@users.noreply.github.com>
2019-08-23 13:35:57 +0200

    updated interface

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request, session, send_file
 from forms import NavigationForm
-from folium import Map, CircleMarker
+from folium import Map, CircleMarker, TileLayer
 from geopy.geocoders import Nominatim
 from bs4 import BeautifulSoup as BS
 from utils import map_to_png, map_to_html, map_to_pdf
@@ -20,11 +20,20 @@ def home():
         # ADD ERROR HANDLING for non valid addresses !
         location = geolocator.geocode(form.search.data)
         coords = (location.latitude, location.longitude)
+        zoom = int(form.zoom.data)
+        map_style = form.map_style.data
+        print('\n\nMAP STYLE: ', map_style)
     else:
         # default coords
         coords = (45.5236, -122.6750)
+        # default zoom
+        zoom = 12
+        map_style = 'stamenterrain'
     # create map
-    map = Map(location=coords, prefer_canvas=True, zoom_start=17)
+    map = Map(location=coords, prefer_canvas=True, zoom_start=zoom)
+
+    TileLayer(map_style).add_to(map)
+
     # add marker for current search result
     CircleMarker(location=coords, radius=12, fill_color='coral', color='red', fill_opacity=0.8).add_to(map)
     soup = BS(map._repr_html_(), features='html.parser')
@@ -34,6 +43,7 @@ def home():
     }
     # add coords to session
     session['coords'] = coords
+    session['zoom'] = zoom
     # render page
     return render_template('index.html', **context)
 
@@ -41,9 +51,10 @@ def home():
 def save_png():
     # get coords from session
     coords = session.get('coords', None)
+    zoom = session.get('zoom', None)
     # create map
     # FIX FIXED ZOOM BUG
-    map = Map(location=coords, prefer_canvas=True, zoom_start=17)
+    map = Map(location=coords, prefer_canvas=True, zoom_start=zoom)
     # save map as png and return path
     path = map_to_png(map)
     # show image of map
@@ -53,9 +64,10 @@ def save_png():
 def save_pdf():
     # get coords from session
     coords = session.get('coords', None)
+    zoom = session.get('zoom', None)
     # create map
     # FIX FIXED ZOOM BUG
-    map = Map(location=coords, prefer_canvas=True, zoom_start=17)
+    map = Map(location=coords, prefer_canvas=True, zoom_start=zoom)
     # create html map and store path
     html_map = map_to_html(map)
     pdf_path = map_to_pdf(html_map)
--- forms.py
+++ forms.py
@@ -1,6 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, IntegerField, SelectField, SubmitField
+
 
 class NavigationForm(FlaskForm):
+
     search = StringField('Search')
-    submit = SubmitField('Go')
+
+    zoom = IntegerField('Zoom')
+
+    map_style = SelectField(
+        'Theme',
+        choices=[
+            ('OpenStreetMap', 'OpenStreetMap'),
+            ('stamenterrain', 'stamenterrain')
+        ]
+    )
+
+    submit = SubmitField('Refresh')
