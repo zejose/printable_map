983632b5ddf53cd3f4fb4a19edf75a8c803e315c
dev <48061063+franzs97@users.noreply.github.com>
2019-08-27 19:39:49 +0200

    updated footer

--- utils.py
+++ utils.py
@@ -15,23 +15,31 @@ def map_to_png(map):
 
     Parameter: map (folium.map)
     '''
+    # html map path
     path = f'usermaps/html/{map.location[0]}_{map.location[1]}.html'
-    print('HTML Path: ', path)
+    
+    # map url
+    tmpurl = f'file://{os.getcwd()}/{path}'
     map.save(path)
-    tmpurl = 'file://{path}/{mapfile}'.format(path=os.getcwd(),mapfile=path)
+
     # Config the Headless Chrome
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--ignore-certificate-errors')
-    # Open a browser window...
+    
+    # Open headless browser...
     browser = webdriver.Chrome((ChromeDriverManager().install()), chrome_options=chrome_options)
+    
     # get page
     browser.get(tmpurl)
+    
     # wait to load page
     time.sleep(1)
+    
     # Grab the screenshot
     png_path = f'usermaps/png/{map.location[0]}_{map.location[1]}.png'
     browser.save_screenshot(png_path)
+    
     # Close the browser
     browser.quit()
     return png_path
@@ -69,7 +77,6 @@ def map_to_pdf(map):
     # closing pdf file 
     file.close() 
     
-    print("\nSuccessfully made pdf file\n") 
     return pdf_path
 
 def map_to_html(map):
@@ -81,21 +88,7 @@ def map_to_html(map):
     Parameter: map (folium.map)
     '''
     path = 'usermaps/html/current_map.html'
+    
     map.save(path)
+
     return path
-
-
-# def map_to_pdf(html_path):
-#     pdf_path = 'usermaps/pdf/current_map.pdf'
-#     # create pdf
-#     options = {
-#         'page-size': 'A4',
-#         'margin-top': '0.75in',
-#         'margin-right': '0.75in',
-#         'margin-bottom': '0.75in',
-#         'margin-left': '0.75in',
-#     }
-#     with open('usermaps/png/map.png', 'r') as image_file:
-#         time.sleep(5)
-#         pdfkit.from_file(image_file, pdf_path, options=options)
-#     return pdf_path
